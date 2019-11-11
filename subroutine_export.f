fe62488e836c1dfd175c5d5e0fb23fe4738efa90
froido <froido@gmx.de>
2019-11-11 06:49:31 +0100

    Indentations corrected

--- subroutine_export.f
+++ subroutine_export.f
@@ -1,7 +1,7 @@
 !	Language: Fortran 95
 !	Compiler: Intel Visual Fortran Compiler
 !	Subroutines for Abaqus 2019
-!	Date: 2019-10
+!	Date: 2019-11
 !	Owner: Sven F. Biebricher - RWTH Aachen, Chair of Geotechnics
 !	Project: MERID
 
@@ -10,6 +10,8 @@
 !	###########################################
 !	Routine that will be executed on every single event you can imagine.
 !	Various execution events are delimited by the parameter analysisPos.
+!     In this case the output file (csv-file) will be created and kept open 
+!     until the end of the end of the analysis.
       subroutine uExternalDB( analysisPos, lRestart, 
 &							time, dTime, kStep, kInc )
 		include 'ABA_PARAM.INC'
@@ -114,134 +116,144 @@
 	end subroutine uExternalDB
 
 
+!	###########################################
+!	URDFIL
+!	###########################################
+!     Routine to access data in the result-file. It will be called at the
+!     end if any increment in which information is written into result-file.
+!     In this analysis the subroutine will be used to read void ratio and
+!     pore pressure from the result-file based on elements. Only the results
+!     (and the coordinates of containing element) of the last increment will
+!     be saved into an output file, opened through subroutine UEXTERNALDB
+!     at the beginning of the analysis.
 	subroutine uRdFil( lStop, lOvrWrt, kStep, kInc, dTime, time)
 
-      include 'ABA_PARAM.INC'
+		include 'ABA_PARAM.INC'
 
-	!	###############################		
-	!	Declare SUBROUTINE variables
-	!	###############################
+		!	###############################		
+		!	Declare SUBROUTINE variables
+		!	###############################
 	
-	integer, dimension(2) :: time
-	integer :: lOverWrt
+		integer, dimension(2) :: time
+		integer :: lOverWrt
 	
-	double precision, dimension(513) :: dpArray
-	integer, dimension(nprecd, 513) :: iArray
+		double precision, dimension(513) :: dpArray
+		integer, dimension(nprecd, 513) :: iArray
 	
-	!	###############################		
-	!	Declare LOCAL variables
-	!	###############################
-	integer, parameter :: fileUnit = 113
+		!	###############################		
+		!	Declare LOCAL variables
+		!	###############################
+		integer, parameter :: fileUnit = 113
 	
-	! Array to collect the data that shall be printed into one
-	! column in the output-file. Size of the array appends
-	! on the data to be stored.
-	! x, y, z, voidRatio, porePressure
-	double precision, dimension(4) :: writeBuffer =-999
+		! Array to collect the data that shall be printed into one
+		! column in the output-file. Size of the array appends
+		! on the data to be stored.
+		! x, y, z, voidRatio, porePressure
+		double precision, dimension(4) :: writeBuffer =-999
 	
-	!Interpretation of double precision as integers
-	equivalence (dpArray(1), iArray(1,1))
+		!Interpretation of double precision as integers
+		equivalence (dpArray(1), iArray(1,1))
 	
-	! Overwrite data in fil-file in next increment
-	lOvrWrt = 1;
+		! Overwrite data in fil-file in next increment
+		lOvrWrt = 1;
 	
-	! The command rewind always jumps back to the beginning of the output file. Thus
-	! only the last written increment is saved effectively.
-	rewind(fileUnit)
+		! The command rewind always jumps back to the beginning of the output file. Thus
+		! only the last written increment is saved effectively.
+		rewind(fileUnit)
 	
-	! Write Headline
-	write(fileUnit, '(A, "; ", A, "; ", A,"; ", A,"; ", A, ";")')
+		! Write Headline
+		write(fileUnit, '(A, "; ", A, "; ", A,"; ", A,"; ", A, ";")')
 &                 'x', 'y', 'z', 'voidRatio', 'porePressure'
 	
-      call posFil( kStep, kInc, dpArray, jrdc)
-      do
-		! Read fil-file
-		call dbFile( 0, dpArray, jrdc)
+		call posFil( kStep, kInc, dpArray, jrdc)
+		do
+			! Read fil-file
+			call dbFile( 0, dpArray, jrdc)
 		
-		! If jrdc is 1 eof is reached
-        if ( jrdc == 1 ) exit
+			! If jrdc is 1 eof is reached
+			if ( jrdc == 1 ) exit
 		
-        key = iArray(1,2)
+			key = iArray(1,2)
 
-		! In the following only element based data will be collected from
-		! the fil-file. Included data will be pore poressure, void ratio
-		! and the coordinates of the integration point of the specific element.
-		! Element datasets are printed out sequentially. Therefore the data
-		! of an element will be collected first and afterwards printed out.
-		! As the pore pressure is just an debug output parameter it will be
-		! saved in another csv-file.
-		isKey: select case (key)
-		! #####################################
-		case (6) ! VOIDRATIO of an ELEMENT
-			voidR = dpArray(3)			
-!			write(fileUnit, '("VoidR: ", F8.6, ";")')
+			! In the following only element based data will be collected from
+			! the fil-file. Included data will be pore poressure, void ratio
+			! and the coordinates of the integration point of the specific element.
+			! Element datasets are printed out sequentially. Therefore the data
+			! of an element will be collected first and afterwards printed out.
+			! As the pore pressure is just an debug output parameter it will be
+			! saved in another csv-file.
+			isKey: select case (key)
+			! #####################################
+			case (6) ! VOIDRATIO of an ELEMENT
+				voidR = dpArray(3)			
+!				write(fileUnit, '("VoidR: ", F8.6, ";")')
 !&                  voidR
 
-		writeBuffer(3) = voidR
+				writeBuffer(3) = voidR
 
 		
-		! #############################
-		case (8) ! COORDINATES of an ELEMENT
-			coord_x = dpArray(3)
-			coord_y = dpArray(4)
-			coord_z = dpArray(5)
+			! #############################
+			case (8) ! COORDINATES of an ELEMENT
+				coord_x = dpArray(3)
+				coord_y = dpArray(4)
+				coord_z = dpArray(5)
 			
-!			write(fileUnit, '("COORD-EL: ", F9.3, ";", F9.3, ";", F9.3, ";")')
+!				write(fileUnit, '("COORD-EL: ", F9.3, ";", F9.3, ";", F9.3, ";")')
 !&                  coord_x, coord_y, coord_z
 
-			writeBuffer(0) = coord_x
-			writeBuffer(1) = coord_y
-			writeBuffer(2) = coord_z
+				writeBuffer(0) = coord_x
+				writeBuffer(1) = coord_y
+				writeBuffer(2) = coord_z
 			
 
-		! #############################
-		case (18)! POREPRESSURE of an ELEMENT
-			porePressure = dpArray(3)
+			! #############################
+			case (18)! POREPRESSURE of an ELEMENT
+				porePressure = dpArray(3)
 			
-!			write(fileUnit, '("POR-EL: ", F, ";")')
+!				write(fileUnit, '("POR-EL: ", F, ";")')
 !&                  porePressure
 
-			writeBuffer(4) = porePressure
+				writeBuffer(4) = porePressure
 			
 
-!		! #############################
-!		case (107) !  COORDINATES of a NODE
-!			nodeNo = iArray(1,3)
-!			coord_x = dpArray(4)
-!			coord_y = dpArray(5)
-!			coord_z = dpArray(6)
+!			! #############################
+!			case (107) !  COORDINATES of a NODE
+!				nodeNo = iArray(1,3)
+!				coord_x = dpArray(4)
+!				coord_y = dpArray(5)
+!				coord_z = dpArray(6)
 !			
-!			write(fileUnit, '("COORD-Node: ", I8, ";", F9.3, ";", F9.3, ";", F9.3, ";")')
+!				write(fileUnit, '("COORD-Node: ", I8, ";", F9.3, ";", F9.3, ";", F9.3, ";")')
 !&                  nodeNo, coord_x, coord_y, coord_z
 		
-!		! #############################
-!		case (108)! POREPRESSURE of a NODE
-!			nodeNo = iArray(1,3)
-!			pore_pressure = dpArray(4)
+!			! #############################
+!			case (108)! POREPRESSURE of a NODE
+!				nodeNo = iArray(1,3)
+!				pore_pressure = dpArray(4)
 !			
-!			write(fileUnit, '("POR-N: ", I8, ";", F, ";")')
+!				write(fileUnit, '("POR-N: ", I8, ";", F, ";")')
 !&                  nodeNo, pore_pressure
 
-!		case default
-!			write(fileUnit, '("KEY: ", I4 ";")')
+!			case default
+!				write(fileUnit, '("KEY: ", I4 ";")')
 !&                  key
 	
-		end select isKey
+			end select isKey
 		
-		if (writeBuffer(0) /= -999 .and. writeBuffer(1) /= -999 .and. 
-&			writeBuffer(2) /= -999 .and. writeBuffer(3) /= -999 .and. 
-&			writeBuffer(4) /= -999) then
+			if (writeBuffer(0) /= -999 .and. writeBuffer(1) /= -999 .and. 
+&				writeBuffer(2) /= -999 .and. writeBuffer(3) /= -999 .and. 
+&				writeBuffer(4) /= -999) then
 			! x, y, z, voidRatio, porePressure
-			write(fileUnit, '(F9.3, "; ", F9.3, "; ", F9.3, "; ",
+				write(fileUnit, '(F9.3, "; ", F9.3, "; ", F9.3, "; ",
 &							F9.7, "; ", F12.2, ";")') writeBuffer(0),
 &							writeBuffer(1), writeBuffer(2), writeBuffer(3), writeBuffer(4)
 			
 			! Reset the writebuffer
-			writeBuffer = -999
-		end if
+				writeBuffer = -999
+			end if
 		
-      end do
+		end do
 	
-      return
-      end
+		return
+      end subroutine uRdFil
 
